8f607bf3d406f5eafb09768c5faab5209f40df0d
Javier Alcoriza <jalcoriza@gmail.com>
2017-08-26 17:40:58 +0200

    Added a new web server, using an html page that does not work so far

---
+++ Samples/Web/MyFirstNodeJsServer/app2.js
@@ -0,0 +1,23 @@
+// run the web server using:
+// $ node app2.js
+
+const http = require('http');
+
+// Change th IP to be accesible from another machine at the local network
+//const hostname = '127.0.0.1';
+const hostname = '192.168.1.63';
+const port = 3001;
+
+var fs = require('fs');
+var index = fs.readFileSync('/home/jalcoriza/Projects/KIOSK/Samples/HTML/test.html');
+
+
+const server = http.createServer((req, res) => {
+	  res.statusCode = 200;
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.end(index);
+});
+
+server.listen(port, hostname, () => {
+	  console.log(`Server running at http://${hostname}:${port}/`);
+	  });
